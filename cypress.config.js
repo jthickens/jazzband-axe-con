87f4169893a6a50c26d7e6dfff50e6e8c8d22d08
Jonathan Thickens <47118049+jthickens@users.noreply.github.com>
2023-03-09 14:31:38 -0500

    chore(release): v2.11.4

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,10 @@
-const { defineConfig } = require("cypress");
-const { cypressConfig } = require('@axe-core/watcher')
 
-// Get your configuration from environment variables.
-const { API_KEY, SERVER_URL } = process.env
+const { cypressConfig } = require('@axe-core/watcher')
 
 module.exports = defineConfig(
   cypressConfig({
     axe: {
-      apiKey: API_KEY,
-      serverURL: SERVER_URL
-    },
-    defaultCommandTimeout: 10000,
-    e2e: {
-      setupNodeEvents(on, config) {
-        // implement node event listeners here
-      },
+      apiKey: 'abcdef-1234',
     },
   })
 );
