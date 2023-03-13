e67428fbc13ca79ea3dcfcb305664b8dccf6c8c8
Jonathan Thickens <47118049+jthickens@users.noreply.github.com>
2023-03-13 09:46:53 -0400

    feat(test): add a11y integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
-
-const { cypressConfig } = require('@axe-core/watcher')
+const { defineConfig } = require('cypress');
+const { cypressConfig } = require('@axe-core/watcher');
 
 module.exports = defineConfig(
   cypressConfig({
     axe: {
-      apiKey: 'abcdef-1234',
-    },
+      apiKey: 'fe734ad2-2e6b-4c94-8714-5653bdad3464',
+      serverURL: 'https://axe.dequelabs.com'
+    }
   })
 );
